58eb6d5807afda63bd51bf5e70bec46744b71d4c
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-08-19 14:25:18 +0200

    RandomNumber() made

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -16,6 +16,20 @@ namespace Integrian2D
 			delete pData;
 			pData = nullptr;
 		}
+
+		template<typename FundamentalType, typename = std::enable_if_t<std::is_fundamental_v<FundamentalType>>>
+		constexpr FundamentalType RandomNumber(const FundamentalType min, const FundamentalType max)
+		{
+			if constexpr (std::is_floating_point_v<FundamentalType>)
+			{
+				// https://stackoverflow.com/questions/686353/random-float-number-generation
+				return min + static_cast<FundamentalType>(rand()) / (static_cast<FundamentalType>(RAND_MAX / (max - min)));
+			}
+			else
+			{
+				return static_cast<FundamentalType>(rand()) % max + min;
+			}
+		}
 	}
 }
 
