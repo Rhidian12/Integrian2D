5f3f77e61050b9d1927b4ecc6a1eceb72b3de068
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-08-20 11:10:42 +0200

    made some typedefines

---
+++ Math/TypeDefines.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef INTEGRIAN2D_TYPEDEFINES_H
+#define INTEGRIAN2D_TYPEDEFINES_H
+
+#include "Point2.h"
+#include "Vector2.h"
+
+namespace Integrian2D
+{
+	using Point2f = Point<2, float>;
+	using Vector2f = Vector<2, float>;
+}
+
+#endif // !INTEGRIAN2D_TYPEDEFINES_H
