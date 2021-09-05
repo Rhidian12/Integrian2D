ee5d049e8c456db859411d886666c23d5f8bae32
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-09-05 17:04:22 +0200

    more testing

--- TestScene.h
+++ TestScene.h
@@ -24,7 +24,7 @@ public:
 		AddGameObject("Test", m_pGameObject);
 
 		m_pGameObject->pTransform->Translate({ 50.f,50.f });
-		m_pGameObject->pTransform->Rotate(Integrian2D::Utils::45.f);
+		m_pGameObject->pTransform->Rotate(Integrian2D::Utils::ToRadians(45.f));
 	}
 
 	virtual void Update() override
