a2c54a9333b413899d821882e42d399e6c38773b
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-09-06 19:21:31 +0200

    removed useless includes from Point

--- Math/Point.h
+++ Math/Point.h
@@ -1,9 +1,6 @@
 #pragma once
 
-#include <limits> // std::numeric_limits
-#include <cstdint>
 #include "Vector.h"
-#include "../Utils/Utils.h"
 
 namespace Integrian2D
 {
@@ -21,6 +18,7 @@ namespace Integrian2D
 		const Vector<P, Type> vector{ p1, p2 };
 		return static_cast<Type>(MagnitudeSquared(vector));
 	}
+
 	template<int P, typename Type>
 	Type Distance(const Point<P, Type>& p1, const Point<P, Type>& p2) noexcept
 	{
