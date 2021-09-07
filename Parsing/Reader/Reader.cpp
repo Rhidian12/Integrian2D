0e03150fc6c86ddb453d3282d5b10697600eb15b
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-09-07 10:33:09 +0200

    Reader made

---
+++ Parsing/Reader/Reader.cpp
@@ -0,0 +1,66 @@
+#include "Reader.h"
+#include <algorithm>
+
+namespace Integrian2D
+{
+	Reader::Reader(const std::string filePath)
+		: m_File{}
+	{
+		m_File.open(filePath, std::ios::in);
+	}
+
+	Reader::~Reader()
+	{
+		m_File.close();
+	}
+
+	void Reader::Open(const std::string filePath) noexcept
+	{
+		m_File.open(filePath, std::ios::in);
+	}
+
+	void Reader::Close() noexcept
+	{
+		m_File.close();
+	}
+
+	void Reader::SetLine(const unsigned int line) noexcept
+	{
+		ASSERT(line < GetAmountOfLines(), "Reader::SetLine() > The line index is out of bounds!");
+
+		m_File.seekg(0); // set to start of file
+
+		for (unsigned int i{}; i < line; ++i)
+		{
+			std::string temp{};
+			std::getline(m_File, temp);
+		}
+	}
+
+	int Reader::GetAmountOfLines() noexcept
+	{
+		// Reference: https://stackoverflow.com/questions/3482064/counting-the-number-of-lines-in-a-text-file
+	
+		int lineCount{};
+
+		while (m_File.peek() != std::ios::eofbit)
+		{
+			std::string temp{};
+			std::getline(m_File, temp);
+
+			++lineCount;
+		}
+
+		return lineCount;
+	}
+
+	std::string Reader::GetLine(const unsigned int line) noexcept
+	{
+		SetLine(line);
+
+		std::string temp{};
+		std::getline(m_File, temp);
+
+		return temp;
+	}
+}
---
+++ Parsing/Reader/Reader.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <type_traits>
+
+#include "../../Utils/Utils.h"
+
+namespace Integrian2D
+{
+	class Reader final
+	{
+	public:
+		Reader(const std::string filePath);
+		~Reader();
+
+		void Open(const std::string filePath) noexcept;
+		void Close() noexcept;
+
+		void SetLine(const unsigned int line) noexcept;
+
+		template<typename Type>
+		void Read() const noexcept
+		{
+			ASSERT(m_File.is_open(), "Reader::Read() > File is not open!");
+
+			Type data{};
+
+			m_File >> data;
+
+			return data;
+		}
+
+		int GetAmountOfLines() noexcept;
+		std::string GetLine(const unsigned int line) noexcept;
+
+	private:
+		std::fstream m_File;
+	};
+}
