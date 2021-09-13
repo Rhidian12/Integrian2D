54d2f8d06aee3f1723fb240fb61269ad13765e84
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-09-13 12:06:47 +0200

    removed physics testing

--- TestScene.h
+++ TestScene.h
@@ -16,7 +16,6 @@ public:
 	TestScene(std::string name)
 		: Scene{ name }
 		, m_pGameObject{ new Integrian2D::GameObject{} }
-		, m_pGameObject2{ new Integrian2D::GameObject{} }
 	{
 		Integrian2D::TextureManager::GetInstance()->AddTexture("DinoHappy", new Integrian2D::Texture{ "dinoHappy.png" });
 	}
@@ -26,63 +25,12 @@ public:
 		using namespace Integrian2D;
 
 		m_pGameObject->AddComponent(new TextureComponent{ m_pGameObject, TextureManager::GetInstance()->GetTexture("DinoHappy") });
-		m_pGameObject->AddComponent(new RectColliderComponent{ m_pGameObject, PRectf{ 100.f,100.f, 152.f, 166.f }, true });
-		m_pGameObject->AddComponent(new PhysicsComponent{ m_pGameObject, 1.f, Vector2f{}, m_pGameObject->GetComponentByType<RectColliderComponent>() });
-
-		AddGameObject("Dino", m_pGameObject);
-
-		m_pGameObject->pTransform->Translate(Integrian2D::Vector2f{ 100.f,100.f });
-
-		inputManager.AddCommand(Integrian2D::GameInput{ Integrian2D::KeyboardInput::Space }, [this]()->void
-			{
-				m_pGameObject->pTransform->Rotate(Integrian2D::Utils::ToRadians(45.f));
-			}, Integrian2D::State::OnRelease);
-
-		inputManager.AddCommand(Integrian2D::GameInput{ Integrian2D::KeyboardInput::ArrowUp }, [this]()->void
-			{
-				m_pGameObject->GetComponentByType<PhysicsComponent>()->AddForce(Integrian2D::Vector2f{ 0.f, 5.f });
-			}, Integrian2D::State::OnHeld);
-
-		inputManager.AddCommand(Integrian2D::GameInput{ Integrian2D::KeyboardInput::ArrowDown }, [this]()->void
-			{
-				m_pGameObject->GetComponentByType<PhysicsComponent>()->AddForce(Integrian2D::Vector2f{ 0.f, -5.f });
-			}, Integrian2D::State::OnHeld);
-
-		inputManager.AddCommand(Integrian2D::GameInput{ Integrian2D::KeyboardInput::ArrowLeft }, [this]()->void
-			{
-				m_pGameObject->GetComponentByType<PhysicsComponent>()->AddForce(Integrian2D::Vector2f{ -5.f, 0.f });
-			}, Integrian2D::State::OnHeld);
-
-		inputManager.AddCommand(Integrian2D::GameInput{ Integrian2D::KeyboardInput::ArrowRight }, [this]()->void
-			{
-				m_pGameObject->GetComponentByType<PhysicsComponent>()->AddForce(Integrian2D::Vector2f{ 5.f, 0.f });
-			}, Integrian2D::State::OnHeld);
-
-		inputManager.AddCommand(Integrian2D::GameInput{ Integrian2D::KeyboardInput::Q }, [this]()->void
-			{
-				m_pGameObject->pTransform->Scale(Integrian2D::Point2f{ 1.f, 1.f });
-			}, Integrian2D::State::OnRelease);
-
-		inputManager.AddCommand(Integrian2D::GameInput{ Integrian2D::KeyboardInput::A }, [this]()->void
-			{
-				m_pGameObject->pTransform->Scale(Integrian2D::Point2f{ -1.f, -1.f });
-			}, Integrian2D::State::OnRelease);
-
-		m_pGameObject2->AddComponent(new TextureComponent{ m_pGameObject2, TextureManager::GetInstance()->GetTexture("DinoHappy") });
-		m_pGameObject2->AddComponent(new RectColliderComponent{ m_pGameObject2, PRectf{ 500.f, 500.f, 152.f, 166.f }, true });
-		m_pGameObject2->AddComponent(new PhysicsComponent{ m_pGameObject2, 0.f, Vector2f{}, m_pGameObject2->GetComponentByType<RectColliderComponent>() });
-
-		AddGameObject("Dino2", m_pGameObject2);
-
-		m_pGameObject2->pTransform->Translate(Vector2f{ 300.f, 300.f });
 	}
 
 	virtual void Update() override
 	{
-		std::cout << "Angle: " << Integrian2D::Utils::ToDegrees(m_pGameObject->pTransform->GetAngle()) << std::endl;
 	}
 
 	Integrian2D::GameObject* m_pGameObject;
-	Integrian2D::GameObject* m_pGameObject2;
 };
 #pragma once
