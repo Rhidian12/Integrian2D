1d636a43409dd7e9d85e1cbfc14455b06dc88726
Rhidian De Wit <rhidiandewit@gmail.com>
2021-09-21 16:11:56 +0200

    added a boplean for gravity enabling to PhysicsInfo

--- PhysicsEngine/PhysicsInfo/PhysicsInfo.cpp
+++ PhysicsEngine/PhysicsInfo/PhysicsInfo.cpp
@@ -4,43 +4,10 @@
 
 namespace Integrian2D
 {
-	PhysicsInfo::PhysicsInfo(const float _mass, const Vector2f _velocity, ColliderComponent* const _pHitbox)
-		: mass{ _mass }
+	PhysicsInfo::PhysicsInfo(const float _mass, const Vector2f _velocity, ColliderComponent* const _pHitbox, const bool _gravity)
+		: gravity{ _gravity }
+		, mass{ _mass }
 		, velocity{ _velocity }
 		, pHitbox{ _pHitbox }
 	{}
-
-	PhysicsInfo::PhysicsInfo(const PhysicsInfo& other) noexcept
-		: mass{ other.mass }
-		, velocity{ other.velocity }
-		, pHitbox{ other.pHitbox }
-	{}
-
-	PhysicsInfo::PhysicsInfo(PhysicsInfo&& other) noexcept
-		: mass{ std::move(other.mass) }
-		, velocity{ std::move(other.velocity) }
-		, pHitbox{ std::move(other.pHitbox) }
-	{
-		other.pHitbox = nullptr;
-	}
-
-	PhysicsInfo& PhysicsInfo::operator=(const PhysicsInfo& other) noexcept
-	{
-		mass = other.mass;
-		velocity = other.velocity;
-		pHitbox = other.pHitbox;
-
-		return *this;
-	}
-
-	PhysicsInfo& PhysicsInfo::operator=(PhysicsInfo&& other) noexcept
-	{
-		mass = std::move(other.mass);
-		velocity = std::move(other.velocity);
-		pHitbox = std::move(other.pHitbox);
-
-		other.pHitbox = nullptr;
-
-		return *this;
-	}
 }
--- PhysicsEngine/PhysicsInfo/PhysicsInfo.h
+++ PhysicsEngine/PhysicsInfo/PhysicsInfo.h
@@ -10,15 +10,16 @@ namespace Integrian2D
 	class PhysicsInfo final
 	{
 	public:
-		PhysicsInfo(const float _mass, const Vector2f _velocity, ColliderComponent* const _pHitbox);
+		PhysicsInfo(const float _mass, const Vector2f _velocity, ColliderComponent* const _pHitbox, const bool _gravity = true);
 
 #pragma region Rule Of 5
-		PhysicsInfo(const PhysicsInfo& other) noexcept;
-		PhysicsInfo(PhysicsInfo&& other) noexcept;
-		PhysicsInfo& operator=(const PhysicsInfo& other) noexcept;
-		PhysicsInfo& operator=(PhysicsInfo&& other) noexcept;
+		PhysicsInfo(const PhysicsInfo& other) = default;
+		PhysicsInfo(PhysicsInfo&& other) = default;
+		PhysicsInfo& operator=(const PhysicsInfo& other) = default;
+		PhysicsInfo& operator=(PhysicsInfo&& other) = default;
 #pragma endregion
 
+		bool gravity;
 		float mass;
 		Vector2f velocity;
 		ColliderComponent* pHitbox;
