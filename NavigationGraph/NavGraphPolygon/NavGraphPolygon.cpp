87ffd05f3256a5ede3c321b5799616e90f3f2f8f
Rhidian De Wit <rhidiandewit@gmail.com>
2021-09-22 20:27:51 +0200

    Started NavGraphPolyon

--- NavigationGraph/NavGraphPolygon/NavGraphPolygon.cpp
+++ NavigationGraph/NavGraphPolygon/NavGraphPolygon.cpp
@@ -1,1 +1,55 @@
 #include "NavGraphPolygon.h"
+#include <algorithm>
+
+namespace Integrian2D
+{
+	NavGraphPolygon::NavGraphPolygon(const std::vector<Point2f> vertices)
+		: m_Vertices{ vertices }
+		, m_ChildPolygons{}
+	{}
+
+	NavGraphPolygon::NavGraphPolygon(const std::vector<Point2f> outerVertices, const std::vector<std::vector<Point2f>> innerVertices)
+		: m_Vertices{ outerVertices }
+		, m_ChildPolygons{}
+	{
+		for (const std::vector<Point2f>& vector : innerVertices)
+			AddChild(vector);
+	}
+
+	NavGraphPolygon* NavGraphPolygon::AddChild(const std::vector<Point2f> vertices) noexcept
+	{
+		m_ChildPolygons.push_back(NavGraphPolygon{ vertices });
+		return &m_ChildPolygons.back();
+	}
+
+	void NavGraphPolygon::AddChild(const NavGraphPolygon child) noexcept
+	{
+		m_ChildPolygons.push_back(child);
+	}
+
+	void NavGraphPolygon::RemoveChild(const NavGraphPolygon& childToRemove) noexcept
+	{
+		m_ChildPolygons.erase(std::remove(m_ChildPolygons.begin(), m_ChildPolygons.end(), childToRemove), m_ChildPolygons.end());
+	}
+
+	const Point2f NavGraphPolygon::GetCenter() const noexcept
+	{
+		Point2f average{};
+
+		for (const Point2f& p : m_Vertices)
+			average += p;
+
+		return average / static_cast<float>(m_Vertices.size());
+	}
+
+	const std::vector<Point2f>& NavGraphPolygon::GetVertices() const noexcept
+	{
+		return m_Vertices;
+	}
+
+	const std::vector<NavGraphPolygon>& NavGraphPolygon::GetChildren() const noexcept
+	{
+		return m_ChildPolygons;
+	}
+
+}
--- NavigationGraph/NavGraphPolygon/NavGraphPolygon.h
+++ NavigationGraph/NavGraphPolygon/NavGraphPolygon.h
@@ -1,8 +1,37 @@
 #pragma once
 
+#include "../../Math/TypeDefines.h"
+#include <vector>
+
 namespace Integrian2D
 {
-	class NavGraphPolygon
+	class NavGraphPolygon final
 	{
+	public:
+		NavGraphPolygon(const std::vector<Point2f> vertices);
+		NavGraphPolygon(const std::vector<Point2f> outerVertices, const std::vector<std::vector<Point2f>> innerVertices);
+
+		NavGraphPolygon* AddChild(const std::vector<Point2f> vertices) noexcept;
+		void AddChild(const NavGraphPolygon child) noexcept;
+		void RemoveChild(const NavGraphPolygon& childToRemove) noexcept;
+
+		const Point2f GetCenter() const noexcept;
+		const std::vector<Point2f>& GetVertices() const noexcept;
+		const std::vector<NavGraphPolygon>& GetChildren() const noexcept;
+
+	private:
+		std::vector<Point2f> m_Vertices; // outer vertices of the polygon
+		std::vector<NavGraphPolygon> m_ChildPolygons; // inner vertices of the polygon
+		// TODO: Make a std::vector of Trianglef (Polygon<3, float>)
 	};
+
+	inline bool operator==(const NavGraphPolygon& a, const NavGraphPolygon& b) noexcept
+	{
+		return a.GetVertices() == b.GetVertices() && a.GetChildren() == b.GetChildren();
+	}
+
+	inline bool operator!=(const NavGraphPolygon& a, const NavGraphPolygon& b) noexcept
+	{
+		return !(a == b);
+	}
 }
