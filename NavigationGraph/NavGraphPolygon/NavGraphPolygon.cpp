bbc23e7e43ab94793c16c5eb3b8a9198466db6e5
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-09-28 16:29:42 +0200

    did more work on the NavGraphPolygon

--- NavigationGraph/NavGraphPolygon/NavGraphPolygon.cpp
+++ NavigationGraph/NavGraphPolygon/NavGraphPolygon.cpp
@@ -1,16 +1,22 @@
 #include "NavGraphPolygon.h"
 #include <algorithm>
+#include <limits>
+
+#undef min // THESE MACROS ARE THE WORST
+#undef max // THESE MACROS ARE THE WORST
 
 namespace Integrian2D
 {
-	NavGraphPolygon::NavGraphPolygon(const std::vector<Point2f> vertices)
+	NavGraphPolygon::NavGraphPolygon(const std::vector<Point2f>& vertices)
 		: m_Vertices{ vertices }
 		, m_ChildPolygons{}
+		, m_IsTriangulated{}
 	{}
 
-	NavGraphPolygon::NavGraphPolygon(const std::vector<Point2f> outerVertices, const std::vector<std::vector<Point2f>> innerVertices)
+	NavGraphPolygon::NavGraphPolygon(const std::vector<Point2f>& outerVertices, const std::vector<std::vector<Point2f>>& innerVertices)
 		: m_Vertices{ outerVertices }
 		, m_ChildPolygons{}
+		, m_IsTriangulated{}
 	{
 		for (const std::vector<Point2f>& vector : innerVertices)
 			AddChild(vector);
@@ -32,7 +38,7 @@ namespace Integrian2D
 		m_ChildPolygons.erase(std::remove(m_ChildPolygons.begin(), m_ChildPolygons.end(), childToRemove), m_ChildPolygons.end());
 	}
 
-	const Point2f NavGraphPolygon::GetCenter() const noexcept
+	Point2f NavGraphPolygon::GetCenter() const noexcept
 	{
 		Point2f average{};
 
@@ -40,6 +46,69 @@ namespace Integrian2D
 			average += p;
 
 		return average / static_cast<float>(m_Vertices.size());
+	}
+
+	bool NavGraphPolygon::IsTriangulated() const noexcept
+	{
+		return m_IsTriangulated;
+	}
+
+	float NavGraphPolygon::GetMaxXVertex() const noexcept
+	{
+		float maxXPosition{ std::numeric_limits<float>::min() };
+		
+		for (const Point2f& position : m_Vertices)
+			if (position.x > maxXPosition)
+				maxXPosition = position.x;
+
+		return maxXPosition;
+	}
+
+	float NavGraphPolygon::GetMaxYVertex() const noexcept
+	{
+		float maxYPosition{ std::numeric_limits<float>::min() };
+
+		for (const Point2f& position : m_Vertices)
+			if (position.y > maxYPosition)
+				maxYPosition = position.y;
+
+		return maxYPosition;
+	}
+
+	float NavGraphPolygon::GetMinXVertex() const noexcept
+	{
+		float minXPosition{ std::numeric_limits<float>::max() };
+
+		for (const Point2f& position : m_Vertices)
+			if (position.x < minXPosition)
+				minXPosition = position.x;
+
+		return minXPosition;
+	}
+
+	float NavGraphPolygon::GetMinYVertex() const noexcept
+	{
+		float minYPosition{ std::numeric_limits<float>::max() };
+
+		for (const Point2f& position : m_Vertices)
+			if (position.y < minYPosition)
+				minYPosition = position.y;
+
+		return minYPosition;
+	}
+
+	bool NavGraphPolygon::IsOverlapping(const NavGraphPolygon& polygon) const noexcept
+	{
+		// == Check if they CERTAINLY cannot overlap ==
+		if (GetMinXVertex() > polygon.GetMaxXVertex() || GetMaxXVertex() < polygon.GetMinXVertex())
+			return false;
+
+		// == Check if they CERTAINLY cannot overlap ==
+		if (GetMinYVertex() > polygon.GetMaxYVertex() || GetMaxYVertex() < polygon.GetMinYVertex())
+			return false;
+
+
+		return true;
 	}
 
 	const std::vector<Point2f>& NavGraphPolygon::GetVertices() const noexcept
@@ -51,5 +120,4 @@ namespace Integrian2D
 	{
 		return m_ChildPolygons;
 	}
-
 }
--- NavigationGraph/NavGraphPolygon/NavGraphPolygon.h
+++ NavigationGraph/NavGraphPolygon/NavGraphPolygon.h
@@ -8,21 +8,29 @@ namespace Integrian2D
 	class NavGraphPolygon final
 	{
 	public:
-		NavGraphPolygon(const std::vector<Point2f> vertices);
-		NavGraphPolygon(const std::vector<Point2f> outerVertices, const std::vector<std::vector<Point2f>> innerVertices);
+		NavGraphPolygon(const std::vector<Point2f>& vertices);
+		NavGraphPolygon(const std::vector<Point2f>& outerVertices, const std::vector<std::vector<Point2f>>& innerVertices);
 
 		NavGraphPolygon* AddChild(const std::vector<Point2f> vertices) noexcept;
 		void AddChild(const NavGraphPolygon child) noexcept;
 		void RemoveChild(const NavGraphPolygon& childToRemove) noexcept;
 
-		const Point2f GetCenter() const noexcept;
+		Point2f GetCenter() const noexcept;
+		bool IsTriangulated() const noexcept;
+		float GetMaxXVertex() const noexcept;
+		float GetMaxYVertex() const noexcept;
+		float GetMinXVertex() const noexcept;
+		float GetMinYVertex() const noexcept;
+		bool IsOverlapping(const NavGraphPolygon& polygon) const noexcept;
 		const std::vector<Point2f>& GetVertices() const noexcept;
 		const std::vector<NavGraphPolygon>& GetChildren() const noexcept;
 
 	private:
 		std::vector<Point2f> m_Vertices; // outer vertices of the polygon
-		std::vector<NavGraphPolygon> m_ChildPolygons; // inner vertices of the polygon
-		std::vector<PTrianglef> m_Triangles;
+		std::vector<NavGraphPolygon> m_ChildPolygons; // inner polygons of this polygon
+		std::vector<PTrianglef> m_Triangles; // Used for rendering
+
+		bool m_IsTriangulated;
 	};
 
 	inline bool operator==(const NavGraphPolygon& a, const NavGraphPolygon& b) noexcept
