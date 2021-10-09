0cdc04b6a7cc5eeb6967dea05be23f909b2155dc
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-09 15:52:09 +0200

    Basic NavigationGraph made
    testing can begin

--- NavigationGraph/NavigationGraph.cpp
+++ NavigationGraph/NavigationGraph.cpp
@@ -1,1 +1,25 @@
 #include "NavigationGraph.h"
+
+namespace Integrian2D
+{
+	NavigationGraph::NavigationGraph(GameObject* pOwner)
+		: Component{ pOwner }
+		, m_Polygons{}
+	{}
+	
+	NavigationGraph::NavigationGraph(GameObject* pOwner, const std::vector<NavGraphPolygon>& polygons)
+		: Component{ pOwner }
+		, m_Polygons{ polygons }
+	{}
+
+	Component* NavigationGraph::Clone(GameObject* pOwner) noexcept
+	{
+		return new NavigationGraph{ pOwner, m_Polygons };
+	}
+
+	void NavigationGraph::Render() const
+	{
+		for (const NavGraphPolygon& polygon : m_Polygons)
+			polygon.Render();
+	}
+}
--- NavigationGraph/NavigationGraph.h
+++ NavigationGraph/NavigationGraph.h
@@ -1,5 +1,23 @@
 #pragma once
-class NavigationGraph
-{
-};
 
+#include "../Components/Component/Component.h"
+#include "NavGraphPolygon/NavGraphPolygon.h"
+
+#include <vector>
+
+namespace Integrian2D
+{
+	class NavigationGraph final : public Component
+	{
+	public:
+		NavigationGraph(GameObject* pOwner);
+		NavigationGraph(GameObject* pOwner, const std::vector<NavGraphPolygon>& polygons);
+
+		virtual Component* Clone(GameObject* pOwner) noexcept override;
+
+		virtual void Render() const override;
+
+	private:
+		std::vector<NavGraphPolygon> m_Polygons;
+	};
+}
