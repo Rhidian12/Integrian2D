de08507d8bda3d0abba4b58e2d07cc50f024dbec
Rhidian De Wit <rhidiandewit@gmail.com>
2021-10-14 09:17:18 +0200

    Added more functionality to NavigationGraph

--- NavigationGraph/NavigationGraph.cpp
+++ NavigationGraph/NavigationGraph.cpp
@@ -1,15 +1,19 @@
 #include "NavigationGraph.h"
 
+#include <algorithm>
+
 namespace Integrian2D
 {
 	NavigationGraph::NavigationGraph(GameObject* pOwner)
 		: Component{ pOwner }
 		, m_Polygons{}
+		, m_IsPolygonAdded{}
 	{}
 	
 	NavigationGraph::NavigationGraph(GameObject* pOwner, const std::vector<NavGraphPolygon>& polygons)
 		: Component{ pOwner }
 		, m_Polygons{ polygons }
+		, m_IsPolygonAdded{}
 	{}
 
 	Component* NavigationGraph::Clone(GameObject* pOwner) noexcept
@@ -17,9 +21,51 @@ namespace Integrian2D
 		return new NavigationGraph{ pOwner, m_Polygons };
 	}
 
+	void NavigationGraph::Update()
+	{
+		if (m_IsPolygonAdded)
+		{
+			m_IsPolygonAdded = false;
+
+			for (NavGraphPolygon& polygon : m_Polygons)
+				if (!polygon.IsTriangulated())
+					polygon.Triangulate();
+		}
+	}
+
 	void NavigationGraph::Render() const
 	{
 		for (const NavGraphPolygon& polygon : m_Polygons)
 			polygon.Render();
+	}
+
+	void NavigationGraph::AddPolygon(const NavGraphPolygon& polygon, const bool allowDuplicatePolygons) noexcept
+	{
+		if (allowDuplicatePolygons)
+		{
+			m_Polygons.push_back(polygon);
+			m_IsPolygonAdded = true;
+		}
+		else
+		{
+			const std::vector<NavGraphPolygon>::const_iterator cIt{ std::find(m_Polygons.cbegin(), m_Polygons.cend(), polygon) };
+			if (cIt == m_Polygons.cend())
+			{
+				m_Polygons.push_back(polygon);
+				m_IsPolygonAdded = true;
+			}
+		}
+	}
+
+	void NavigationGraph::RemovePolygon(const NavGraphPolygon& polygonToRemove) noexcept
+	{
+		const std::vector<NavGraphPolygon>::const_iterator cIt{ std::find(m_Polygons.cbegin(), m_Polygons.cend(), polygonToRemove) };
+		if (cIt != m_Polygons.cend())
+			m_Polygons.erase(cIt);
+	}
+
+	const std::vector<NavGraphPolygon>& NavigationGraph::GetPolygons() const noexcept
+	{
+		return m_Polygons;
 	}
 }
--- NavigationGraph/NavigationGraph.h
+++ NavigationGraph/NavigationGraph.h
@@ -15,9 +15,16 @@ namespace Integrian2D
 
 		virtual Component* Clone(GameObject* pOwner) noexcept override;
 
+		virtual void Update() override;
 		virtual void Render() const override;
+
+		void AddPolygon(const NavGraphPolygon& polygon, const bool allowDuplicatePolygons = false) noexcept;
+		void RemovePolygon(const NavGraphPolygon& polygonToRemove) noexcept;
+
+		const std::vector<NavGraphPolygon>& GetPolygons() const noexcept;
 
 	private:
 		std::vector<NavGraphPolygon> m_Polygons;
+		bool m_IsPolygonAdded;
 	};
 }
