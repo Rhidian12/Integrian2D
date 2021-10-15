204bac57a1239f33619adbcd1a9658d12132c97b
Rhidian De Wit <rhidiandewit@gmail.com>
2021-10-15 20:27:02 +0200

    Finished EventQueue

--- EventQueue/EventQueue.cpp
+++ EventQueue/EventQueue.cpp
@@ -1,6 +1,52 @@
 #include "EventQueue.h"
+#include "../IListener/IListener.h"
+
+#include <algorithm>
 
 namespace Integrian2D
 {
+	void EventQueue::QueueEvent(const Event& event) noexcept
+	{
+		m_Events.push_back(event);
+	}
 
+	void EventQueue::Update() noexcept
+	{
+		if (!m_Events.empty())
+		{
+			for (Event& event : m_Events)
+				if (event.delay == 0 || event.delayCounter++ >= event.delay) // if there is no delay or the delayCounter has surpassed the expected delay
+					for (IListener* const pListener : m_pListeners)
+						if (pListener->m_IsActive)
+							if (pListener->OnEvent(event))
+								event.isExecuted = true;
+
+			// Erase events that have been executed
+			m_Events.erase(std::remove_if(m_Events.begin(), m_Events.end(), [](const Event& event)->bool
+				{
+					return event.isExecuted;
+				}), m_Events.end());
+		}
+	}
+
+	void EventQueue::AddListener(IListener* const pListener) noexcept
+	{
+		const std::vector<IListener*>::const_iterator cIt{ std::find(m_pListeners.cbegin(), m_pListeners.cend(), pListener) };
+
+		if (cIt == m_pListeners.cend())
+			m_pListeners.push_back(pListener);
+	}
+
+	void EventQueue::RemoveListener(IListener* const pListener) noexcept
+	{
+		const std::vector<IListener*>::const_iterator cIt{ std::find(m_pListeners.cbegin(), m_pListeners.cend(), pListener) };
+
+		if (cIt != m_pListeners.cend())
+			m_pListeners.erase(cIt);
+	}
+
+	bool EventQueue::AreAllEventsProcessed() const noexcept
+	{
+		return m_Events.empty();
+	}
 }
--- EventQueue/EventQueue.h
+++ EventQueue/EventQueue.h
@@ -1,13 +1,32 @@
 #pragma once
+
+#include "Event/Event.h"
+
+#include <vector>
+#include <deque>
+
 namespace Integrian2D
 {
+	class IListener;
+
 	class EventQueue final
 	{
 	public:
+		void QueueEvent(const Event& event) noexcept;
+
+		void Update() noexcept;
+
+		void AddListener(IListener* const pListener) noexcept;
+		void RemoveListener(IListener* const pListener) noexcept;
+
+		bool AreAllEventsProcessed() const noexcept;
 
 	private:
 		friend class Scene;
 
 		EventQueue();
+
+		std::vector<IListener*> m_pListeners;
+		std::deque<Event> m_Events;
 	};
 }
