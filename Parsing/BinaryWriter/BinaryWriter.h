9703892d50685b9aff18b0643ad4d031b93f19af
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-30 16:41:17 +0200

    moved the definition of Write() outside the class

--- Parsing/BinaryWriter/BinaryWriter.h
+++ Parsing/BinaryWriter/BinaryWriter.h
@@ -30,26 +30,29 @@ namespace Integrian2D
 		/* Read data from the binary file
 		   If the requested data is not POD, this function might result in undefined behaviour */
 		template<typename Data>
-		void Write(const Data& data) noexcept
-		{
-			ASSERT(m_File.is_open(), "BinaryWriter could not open the file!");
-
-			typedef std::remove_const_t<std::remove_reference_t<decltype(data)>> removeCVData;
-
-			static_assert(!std::is_same_v<removeCVData, char[]>, "String literals are not supported! Only strings!");
-			
-			if constexpr (std::is_same_v<removeCVData, std::string>)
-			{
-				const size_t size{ data.size() };
-				m_File.write(reinterpret_cast<const char*>(&size), sizeof(size_t));
-				m_File.write(data.c_str(), data.size());
-			}
-			else
-				m_File.write(reinterpret_cast<const char*>(&data), sizeof(Data));
-		}
+		void Write(const Data& data) noexcept;
 
 	private:
 		std::fstream m_File;
 		std::string m_FileName;
 	};
+
+	template<typename Data>
+	void BinaryWriter::Write(const Data& data) noexcept
+	{
+		ASSERT(m_File.is_open(), "BinaryWriter could not open the file!");
+
+		typedef std::remove_const_t<std::remove_reference_t<decltype(data)>> removeCVData;
+
+		static_assert(!std::is_same_v<removeCVData, char[]>, "String literals are not supported! Only strings!");
+
+		if constexpr (std::is_same_v<removeCVData, std::string>)
+		{
+			const size_t size{ data.size() };
+			m_File.write(reinterpret_cast<const char*>(&size), sizeof(size_t));
+			m_File.write(data.c_str(), data.size());
+		}
+		else
+			m_File.write(reinterpret_cast<const char*>(&data), sizeof(Data));
+	}
 }
