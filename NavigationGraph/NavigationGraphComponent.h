152c220a08099ba5dd8f25ee50c0d996690b3c10
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-30 16:13:50 +0200

    added more documentation to NavigationGraphComponent

--- NavigationGraph/NavigationGraphComponent.h
+++ NavigationGraph/NavigationGraphComponent.h
@@ -7,7 +7,8 @@
 
 namespace Integrian2D
 {
-	/* This Component can represent a 2D Polygon as a NavigationGraph */
+	/* This Component can represent a 2D Polygon as a NavigationGraph 
+	   Note that a Polygon must not be a triangle */
 	class NavigationGraphComponent final : public Component
 	{
 	public:
@@ -24,10 +25,14 @@ namespace Integrian2D
 		   the GameObject in question will call this function internally */
 		virtual void Render() const override;
 
-		/* Add a NavGraph Polygon to the Navigation Graph */
+		/* Add a NavGraph Polygon to the Navigation Graph 
+		   Duplicate polygons do not get added */
 		void AddPolygon(const NavGraphPolygon& polygon) noexcept;
+
+		/* Remove a previously added NavGraph Polygon from the Navigation Graph */
 		void RemovePolygon(const NavGraphPolygon& polygonToRemove) noexcept;
 
+		/* Return all added NavGraph Polygon to the Navigation Graph */
 		const std::vector<NavGraphPolygon>& GetPolygons() const noexcept;
 
 	private:
