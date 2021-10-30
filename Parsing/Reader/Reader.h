d716e406b3c308f4ceefbdec8a9e837f59528591
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-30 16:47:43 +0200

    added documentation to the Reader

--- Parsing/Reader/Reader.h
+++ Parsing/Reader/Reader.h
@@ -8,17 +8,28 @@
 
 namespace Integrian2D
 {
+	/* This is a RAII reader to read data from non-binary files */
 	class Reader final
 	{
 	public:
+		/* Opens the non-binary file with path filePath */
 		Reader(const std::string filePath);
 		~Reader();
 
+		/* Open a new non-binary file */
 		void Open(const std::string filePath) noexcept;
+
+		/* Close the non-binary file
+		   This happens automatically on destruction of the reader */
 		void Close() noexcept;
 
+		/* Sets the Reader's internal line number 
+		   E.g. Calling this function with parameter 0 will set the reader to the beginning of the file 
+		   Note that this function has a linear time complexity, the further the line number the longer this takes */
 		void SetLine(const unsigned int line) noexcept;
 
+		/* Read data from the non-binary file
+		   If the requested data is wrong, this function will result in undefined behaviour */
 		template<typename Type>
 		void Read() const noexcept
 		{
@@ -31,7 +42,11 @@ namespace Integrian2D
 			return data;
 		}
 
+		/* Get amount of lines this non-binary file contains 
+		   Note that this function has a linear time complexity */
 		unsigned int GetAmountOfLines() noexcept;
+
+		/* Read a specific line from the file */
 		std::string GetLine(const unsigned int line) noexcept;
 
 	private:
