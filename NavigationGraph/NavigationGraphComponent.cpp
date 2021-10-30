4fb59326a07117a3cdba27f13138d40abfb48327
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-30 16:26:57 +0200

    added a way to decide whether Polygons get triangulated

--- NavigationGraph/NavigationGraphComponent.cpp
+++ NavigationGraph/NavigationGraphComponent.cpp
@@ -11,28 +11,20 @@ namespace Integrian2D
 		, m_Polygons{}
 		, m_IsPolygonAdded{}
 	{}
-	
-	NavigationGraphComponent::NavigationGraphComponent(GameObject* pOwner, const std::vector<NavGraphPolygon>& polygons)
+
+	NavigationGraphComponent::NavigationGraphComponent(GameObject* pOwner, const std::vector<NavGraphPolygon>& polygons, const bool triangulate)
 		: Component{ pOwner }
 		, m_Polygons{ polygons }
 		, m_IsPolygonAdded{}
-	{}
+	{
+		if (triangulate)
+			for (NavGraphPolygon& polygon : m_Polygons)
+				polygon.Triangulate();
+	}
 
 	Component* NavigationGraphComponent::Clone(GameObject* pOwner) noexcept
 	{
 		return new NavigationGraphComponent{ pOwner, m_Polygons };
-	}
-
-	void NavigationGraphComponent::Update()
-	{
-		if (m_IsPolygonAdded)
-		{
-			m_IsPolygonAdded = false;
-
-			for (NavGraphPolygon& polygon : m_Polygons)
-				if (!polygon.IsTriangulated())
-					polygon.Triangulate();
-		}
 	}
 
 	void NavigationGraphComponent::Render() const
@@ -41,14 +33,18 @@ namespace Integrian2D
 			polygon.Render(m_pOwner->pTransform->GetWorldPosition());
 	}
 
-	void NavigationGraphComponent::AddPolygon(const NavGraphPolygon& polygon) noexcept
+	void NavigationGraphComponent::AddPolygon(NavGraphPolygon& polygon, const bool triangulatePolygon) noexcept
 	{
-			const std::vector<NavGraphPolygon>::const_iterator cIt{ std::find(m_Polygons.cbegin(), m_Polygons.cend(), polygon) };
-			if (cIt == m_Polygons.cend())
-			{
-				m_Polygons.push_back(polygon);
-				m_IsPolygonAdded = true;
-			}
+		const std::vector<NavGraphPolygon>::const_iterator cIt{ std::find(m_Polygons.cbegin(), m_Polygons.cend(), polygon) };
+		if (cIt == m_Polygons.cend())
+		{
+			if (triangulatePolygon)
+				if (!polygon.IsTriangulated())
+					polygon.Triangulate();
+
+			m_Polygons.push_back(polygon);
+			m_IsPolygonAdded = true;
+		}
 	}
 
 	void NavigationGraphComponent::RemovePolygon(const NavGraphPolygon& polygonToRemove) noexcept
@@ -56,6 +52,12 @@ namespace Integrian2D
 		const std::vector<NavGraphPolygon>::const_iterator cIt{ std::find(m_Polygons.cbegin(), m_Polygons.cend(), polygonToRemove) };
 		if (cIt != m_Polygons.cend())
 			m_Polygons.erase(cIt);
+	}
+
+	void NavigationGraphComponent::Triangulate() noexcept
+	{
+		for (NavGraphPolygon& polygon : m_Polygons)
+			polygon.Triangulate();
 	}
 
 	const std::vector<NavGraphPolygon>& NavigationGraphComponent::GetPolygons() const noexcept
--- NavigationGraph/NavigationGraphComponent.h
+++ NavigationGraph/NavigationGraphComponent.h
@@ -13,13 +13,9 @@ namespace Integrian2D
 	{
 	public:
 		NavigationGraphComponent(GameObject* pOwner);
-		NavigationGraphComponent(GameObject* pOwner, const std::vector<NavGraphPolygon>& polygons);
+		NavigationGraphComponent(GameObject* pOwner, const std::vector<NavGraphPolygon>& polygons, const bool triangulate = true);
 
 		virtual Component* Clone(GameObject* pOwner) noexcept override;
-
-		/* This function should NOT get called manually. If this component is added to a GameObject,
-		   the GameObject in question will call this function internally */
-		virtual void Update() override;
 
 		/* This function should NOT get called manually. If this component is added to a GameObject,
 		   the GameObject in question will call this function internally */
@@ -27,10 +23,15 @@ namespace Integrian2D
 
 		/* Add a NavGraph Polygon to the Navigation Graph 
 		   Duplicate polygons do not get added */
-		void AddPolygon(const NavGraphPolygon& polygon) noexcept;
+		void AddPolygon(NavGraphPolygon& polygon, const bool triangulatePolygon = true) noexcept;
 
 		/* Remove a previously added NavGraph Polygon from the Navigation Graph */
 		void RemovePolygon(const NavGraphPolygon& polygonToRemove) noexcept;
+
+		/* This triangulates all NavGraphPolygons added to this Navigation Graph 
+		   Note that this function can be very expensive depending on the NavGraphPolygons and the amount of Polygons
+		   This function should not be called every frame */
+		void Triangulate() noexcept;
 
 		/* Return all added NavGraph Polygon to the Navigation Graph */
 		const std::vector<NavGraphPolygon>& GetPolygons() const noexcept;
