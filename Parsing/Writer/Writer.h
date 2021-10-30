3b8bdb44e9c8dcdedb3b7cf5dcf1c5b7b3dd4f1e
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-30 16:57:12 +0200

    added documentation to Writer

--- Parsing/Writer/Writer.h
+++ Parsing/Writer/Writer.h
@@ -8,15 +8,23 @@
 
 namespace Integrian2D
 {
+	/* This is a RAII writer to write data to non-binary files */
 	class Writer final
 	{
 	public:
+		/* Opens the non-binary file with path filePath */
 		Writer(const std::string filePath);
 		~Writer();
 
+		/* Open a new non-binary file */
 		void Open(const std::string filePath) noexcept;
+
+		/* Close the non-binary file
+		   This happens automatically on destruction of the writer */
 		void Close() noexcept;
 
+		/* Write data to the non-binary file
+		   If the requested data has no operator<<, this function will result in a compiler error */
 		template<typename Type>
 		void Write(const Type& data, const bool newLine) noexcept
 		{
