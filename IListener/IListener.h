214211508f47c7d3d080cf477e5714ca1fb5d3d4
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-30 15:57:45 +0200

    Added documentation to the IListener

--- IListener/IListener.h
+++ IListener/IListener.h
@@ -4,14 +4,20 @@
 
 namespace Integrian2D
 {
+	/* This class is supposed to be inherited from 
+	   Any instance of this class gets added to the EventQueue */
 	class IListener
 	{
 	public:
+		/* Upon creation, any instance of this class gets added to the EventQueue */
 		IListener();
 		virtual ~IListener() = default;
 
+		/* The function that receives the events broadcasted by the EventQueue */
 		virtual bool OnEvent(const Event&) = 0;
 
+		/* Check whether or not this IListener is receiving events
+		   This is true by default */
 		bool m_IsActive;
 	};
 }
