63b47b78bb557327b08877982d8b333b78f8931e
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-31 08:48:52 +0100

    added documentation to SceneManager

--- SceneManager/SceneManager.h
+++ SceneManager/SceneManager.h
@@ -9,15 +9,28 @@ namespace Integrian2D
 	class SceneManager final
 	{
 	public:
+		/* Get a SceneManager instance */
 		static SceneManager* const GetInstance() noexcept;
+
+		/* Interal Usage
+		   Do NOT call this manually */
 		static void Cleanup() noexcept;
 
 		~SceneManager();
 
+		/* Adds a Scene to the SceneManager 
+		   Duplicate Scenes do not get added */
 		void AddScene(Scene* const pScene) noexcept;
 		
+		/* Get the currently active scene 
+		   If no scene is marked as the active scene, this will return a nullptr */
 		Scene* const GetActiveScene() const noexcept;
+
+		/* Get the Scene with the corresponding Scene Name */
 		Scene* const GetScene(std::string sceneName) const noexcept;
+
+		/* Get all added Scenes 
+		   If no Scenes have been added, this will return an empty map */
 		const std::unordered_map<std::string, Scene*>& GetScenes() const noexcept;
 
 	private:
