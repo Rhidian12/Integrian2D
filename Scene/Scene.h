0858a9dfe38d37d39680ea41238bdf418745612b
Rhidian De Wit <rhidiandewit@gmail.com>
2021-11-09 15:50:47 +0100

    removeed useless struct

--- Scene/Scene.h
+++ Scene/Scene.h
@@ -20,7 +20,7 @@ namespace Integrian2D
 		Scene(const std::string& sceneName);
 		virtual ~Scene();
 
-		/* This function has to be overridden 
+		/* This function has to be overridden
 		   This is where all of the Scene initisialition should start! */
 		virtual void Start() = 0;
 
@@ -44,7 +44,7 @@ namespace Integrian2D
 		   This happens internally */
 		virtual void Render() const {}
 
-		/* Use this function to add GameObjects to the Scene! 
+		/* Use this function to add GameObjects to the Scene!
 		   GameObjects with duplicate GameObject names are not allowed, except if the shouldAlwaysAdd parameter is set to true
 		   If this boolean is set to true and there is a duplicate GameObject name, the name will be the same, with (N) added to it
 		   where N is the amount of times this name has been added - 1 */
@@ -53,11 +53,11 @@ namespace Integrian2D
 		/* Set the Scene's Name */
 		void SetSceneName(const std::string& sceneName) noexcept;
 
-		/* Get a previously added GameObject by its name 
+		/* Get a previously added GameObject by its name
 		   This function will return a nullptr if the GameObject is not present */
 		GameObject* const GetGameObject(const std::string& gameObjectName) const noexcept;
 
-		/* Get all previously added GameObjects to the Scene 
+		/* Get all previously added GameObjects to the Scene
 		   This function will return an empty map if there have been no added GameObjects */
 		const std::unordered_map<std::string, GameObject*> GetGameObjects() const noexcept;
 
@@ -65,15 +65,9 @@ namespace Integrian2D
 		const std::string& GetSceneName() const noexcept;
 
 	protected:
-#pragma warning ( push )
-#pragma warning ( disable : 4201 ) // Disable nameless struct warning
-		struct
-		{
-			InputManager inputManager{};
-			PhysicsEngine physicsEngine{};
-			EventQueue eventQueue{};
-		};
-#pragma warning ( pop )
+		InputManager inputManager{};
+		PhysicsEngine physicsEngine{};
+		EventQueue eventQueue{};
 
 	private:
 		friend class Core; // Make sure that only Core can access the Root functions
