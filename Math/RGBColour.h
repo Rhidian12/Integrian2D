49b500385c5608f9329ef5fcb1eb8c44c8c36667
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-11-15 17:52:51 +0100

    finished RGBColour

--- Math/RGBColour.h
+++ Math/RGBColour.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 
 #include "../Utils/Utils.h"
+#include "BetterUint8.h"
 
 #ifdef max /* This macro was destroying the entire file */
 #undef max
@@ -12,110 +13,41 @@
 
 namespace Integrian2D
 {
-#pragma region Betteruint8_t
-	struct Betteruint8_t final /* for some strange reason, doing uint8_t + uint8_t returned an integer, so I made this struct to circumvent that issue */
-	{
-		explicit Betteruint8_t(const uint8_t _value)
-			: v{ _value }
-		{}
+	/* This class represents a RGB colour, note that the rgb components range from [0, 255] and the a component ranges from [0, 1] */
 
-		uint8_t v;
+	/* A list of available operators:
 
-		operator uint8_t()
-		{
-			return v;
-		}
-	};
+	   RGBColour operator+(const RGBColour& cOne, const RGBColour& cTwo)
+	   RGBColour operator+(const RGBColour& cOne, const uint8_t inc)
 
-	inline Betteruint8_t operator+(const Betteruint8_t& a, const Betteruint8_t& b) noexcept
-	{
-		return Betteruint8_t(static_cast<uint8_t>(a.v + b.v));
-	}
+	   RGBColour operator-(const RGBColour& cOne, const RGBColour& cTwo)
+	   RGBColour operator-(const RGBColour& cOne, const uint8_t inc)
 
-	inline Betteruint8_t& operator+=(Betteruint8_t& a, const Betteruint8_t& b) noexcept
-	{
-		static_cast<uint8_t>(a.v += b.v);
-		return a;
-	}
+	   RGBColour operator*(const RGBColour& cOne, const RGBColour& cTwo)
+	   RGBColour operator*(const RGBColour& cOne, const uint8_t inc)
 
-	inline Betteruint8_t operator+(const Betteruint8_t& a, const uint8_t b) noexcept
-	{
-		return Betteruint8_t(static_cast<uint8_t>(a.v + b));
-	}
+	   RGBColour operator/(const RGBColour& cOne, const RGBColour& cTwo)
+	   RGBColour operator/(const RGBColour& cOne, const uint8_t inc)
 
-	inline Betteruint8_t& operator+=(Betteruint8_t& a, const uint8_t b) noexcept
-	{
-		static_cast<uint8_t>(a.v += b);
-		return a;
-	}
+	   RGBColour& operator+=(RGBColour& cOne, const RGBColour& cTwo)
+	   RGBColour& operator+=(RGBColour& cOne, const uint8_t inc)
 
-	inline Betteruint8_t operator-(const Betteruint8_t& a, const Betteruint8_t& b) noexcept
-	{
-		return Betteruint8_t(static_cast<uint8_t>(a.v - b.v));
-	}
+	   RGBColour& operator-=(RGBColour& cOne, const RGBColour& cTwo)
+	   RGBColour& operator-=(RGBColour& cOne, const uint8_t inc)
 
-	inline Betteruint8_t& operator-=(Betteruint8_t& a, const Betteruint8_t& b) noexcept
-	{
-		static_cast<uint8_t>(a.v -= b.v);
-		return a;
-	}
+	   RGBColour& operator*=(RGBColour& cOne, const RGBColour& cTwo)
+	   RGBColour& operator*=(RGBColour& cOne, const uint8_t inc)
 
-	inline Betteruint8_t operator-(const Betteruint8_t& a, const uint8_t b) noexcept
-	{
-		return Betteruint8_t(static_cast<uint8_t>(a.v - b));
-	}
+	   RGBColour& operator/=(RGBColour& cOne, const RGBColour& cTwo)
+	   RGBColour& operator/=(RGBColour& cOne, const uint8_t inc)
+	   */
 
-	inline Betteruint8_t& operator-=(Betteruint8_t& a, const uint8_t b) noexcept
-	{
-		static_cast<uint8_t>(a.v -= b);
-		return a;
-	}
-
-	inline Betteruint8_t operator*(const Betteruint8_t& a, const Betteruint8_t& b) noexcept
-	{
-		return Betteruint8_t(static_cast<uint8_t>(a.v * b.v));
-	}
-
-	inline Betteruint8_t& operator*=(Betteruint8_t& a, const Betteruint8_t& b) noexcept
-	{
-		static_cast<uint8_t>(a.v *= b.v);
-		return a;
-	}
-
-	inline Betteruint8_t operator*(const Betteruint8_t& a, const uint8_t b) noexcept
-	{
-		return Betteruint8_t(static_cast<uint8_t>(a.v * b));
-	}
-
-	inline Betteruint8_t& operator*=(Betteruint8_t& a, const uint8_t b) noexcept
-	{
-		static_cast<uint8_t>(a.v *= b);
-		return a;
-	}
-
-	inline Betteruint8_t operator/(const Betteruint8_t& a, const Betteruint8_t& b) noexcept
-	{
-		return Betteruint8_t(static_cast<uint8_t>(a.v / b.v));
-	}
-
-	inline Betteruint8_t& operator/=(Betteruint8_t& a, const Betteruint8_t& b) noexcept
-	{
-		static_cast<uint8_t>(a.v /= b.v);
-		return a;
-	}
-
-	inline Betteruint8_t operator/(const Betteruint8_t& a, const uint8_t b) noexcept
-	{
-		return Betteruint8_t(static_cast<uint8_t>(a.v / b));
-	}
-
-	inline Betteruint8_t& operator/=(Betteruint8_t& a, const uint8_t b) noexcept
-	{
-		static_cast<uint8_t>(a.v /= b);
-		return a;
-	}
-
-#pragma endregion
+	   /* A list of available functions:
+		 
+		 void MaxToOne(RGBColour& colour)
+		 =>		Finds the maximum value of the rgb components, then divides every component by that max value
+		 =>		This function essentially normalizes the colour
+		*/
 
 	struct RGBColour final
 	{
@@ -173,138 +105,138 @@ namespace Integrian2D
 #pragma region Data
 		Betteruint8_t r, g, b, a; // uint8_t has a range of [0, 255]
 #pragma endregion
-};
+	};
 
 #pragma region Arithmetic Operators
-inline RGBColour operator+(const RGBColour& cOne, const RGBColour& cTwo) noexcept
-{
-	return RGBColour{ cOne.r + cTwo.r, cOne.b + cTwo.b, cOne.g + cTwo.g, cOne.a + cTwo.a };
-}
+	inline RGBColour operator+(const RGBColour& cOne, const RGBColour& cTwo) noexcept
+	{
+		return RGBColour{ cOne.r + cTwo.r, cOne.b + cTwo.b, cOne.g + cTwo.g, cOne.a + cTwo.a };
+	}
 
-inline RGBColour operator+(const RGBColour& cOne, const uint8_t inc) noexcept
-{
-	return RGBColour{ cOne.r + inc, cOne.b + inc, cOne.g + inc, cOne.a + inc };
-}
+	inline RGBColour operator+(const RGBColour& cOne, const uint8_t inc) noexcept
+	{
+		return RGBColour{ cOne.r + inc, cOne.b + inc, cOne.g + inc, cOne.a + inc };
+	}
 
-inline RGBColour operator-(const RGBColour& cOne, const RGBColour& cTwo) noexcept
-{
-	return RGBColour{ cOne.r - cTwo.r, cOne.b - cTwo.b, cOne.g - cTwo.g, cOne.a - cTwo.a };
-}
+	inline RGBColour operator-(const RGBColour& cOne, const RGBColour& cTwo) noexcept
+	{
+		return RGBColour{ cOne.r - cTwo.r, cOne.b - cTwo.b, cOne.g - cTwo.g, cOne.a - cTwo.a };
+	}
 
-inline RGBColour operator-(const RGBColour& cOne, const uint8_t inc) noexcept
-{
-	return RGBColour{ cOne.r - inc, cOne.b - inc, cOne.g - inc, cOne.a - inc };
-}
+	inline RGBColour operator-(const RGBColour& cOne, const uint8_t inc) noexcept
+	{
+		return RGBColour{ cOne.r - inc, cOne.b - inc, cOne.g - inc, cOne.a - inc };
+	}
 
-inline RGBColour operator*(const RGBColour& cOne, const RGBColour& cTwo) noexcept
-{
-	return RGBColour{ cOne.r * cTwo.r, cOne.b * cTwo.b, cOne.g * cTwo.g, cOne.a * cTwo.a };
-}
+	inline RGBColour operator*(const RGBColour& cOne, const RGBColour& cTwo) noexcept
+	{
+		return RGBColour{ cOne.r * cTwo.r, cOne.b * cTwo.b, cOne.g * cTwo.g, cOne.a * cTwo.a };
+	}
 
-inline RGBColour operator*(const RGBColour& cOne, const uint8_t inc) noexcept
-{
-	return RGBColour{ cOne.r * inc, cOne.b * inc, cOne.g * inc, cOne.a * inc };
-}
+	inline RGBColour operator*(const RGBColour& cOne, const uint8_t inc) noexcept
+	{
+		return RGBColour{ cOne.r * inc, cOne.b * inc, cOne.g * inc, cOne.a * inc };
+	}
 
-inline RGBColour operator/(const RGBColour& cOne, const RGBColour& cTwo) noexcept
-{
-	return RGBColour{ cOne.r / cTwo.r, cOne.b / cTwo.b, cOne.g / cTwo.g, cOne.a / cTwo.a };
-}
+	inline RGBColour operator/(const RGBColour& cOne, const RGBColour& cTwo) noexcept
+	{
+		return RGBColour{ cOne.r / cTwo.r, cOne.b / cTwo.b, cOne.g / cTwo.g, cOne.a / cTwo.a };
+	}
 
-inline RGBColour operator/(const RGBColour& cOne, const uint8_t inc) noexcept
-{
-	return RGBColour{ cOne.r / inc, cOne.b / inc, cOne.g / inc, cOne.a / inc };
-}
+	inline RGBColour operator/(const RGBColour& cOne, const uint8_t inc) noexcept
+	{
+		return RGBColour{ cOne.r / inc, cOne.b / inc, cOne.g / inc, cOne.a / inc };
+	}
 #pragma endregion
 
 #pragma region Compound Arithmetic Operators
-inline RGBColour& operator+=(RGBColour& cOne, const RGBColour& cTwo) noexcept
-{
-	cOne.r += cTwo.r;
-	cOne.g += cTwo.g;
-	cOne.b += cTwo.b;
-	cOne.a += cTwo.a;
+	inline RGBColour& operator+=(RGBColour& cOne, const RGBColour& cTwo) noexcept
+	{
+		cOne.r += cTwo.r;
+		cOne.g += cTwo.g;
+		cOne.b += cTwo.b;
+		cOne.a += cTwo.a;
 
-	return cOne;
-}
+		return cOne;
+	}
 
-inline RGBColour& operator+=(RGBColour& cOne, const uint8_t inc) noexcept
-{
-	cOne.r += inc;
-	cOne.g += inc;
-	cOne.b += inc;
-	cOne.a += inc;
+	inline RGBColour& operator+=(RGBColour& cOne, const uint8_t inc) noexcept
+	{
+		cOne.r += inc;
+		cOne.g += inc;
+		cOne.b += inc;
+		cOne.a += inc;
 
-	return cOne;
-}
+		return cOne;
+	}
 
-inline RGBColour& operator-=(RGBColour& cOne, const RGBColour& cTwo) noexcept
-{
-	cOne.r -= cTwo.r;
-	cOne.g -= cTwo.g;
-	cOne.b -= cTwo.b;
-	cOne.a -= cTwo.a;
+	inline RGBColour& operator-=(RGBColour& cOne, const RGBColour& cTwo) noexcept
+	{
+		cOne.r -= cTwo.r;
+		cOne.g -= cTwo.g;
+		cOne.b -= cTwo.b;
+		cOne.a -= cTwo.a;
 
-	return cOne;
-}
+		return cOne;
+	}
 
-inline RGBColour& operator-=(RGBColour& cOne, const uint8_t inc) noexcept
-{
-	cOne.r -= inc;
-	cOne.g -= inc;
-	cOne.b -= inc;
-	cOne.a -= inc;
+	inline RGBColour& operator-=(RGBColour& cOne, const uint8_t inc) noexcept
+	{
+		cOne.r -= inc;
+		cOne.g -= inc;
+		cOne.b -= inc;
+		cOne.a -= inc;
 
-	return cOne;
-}
+		return cOne;
+	}
 
-inline RGBColour& operator*(RGBColour& cOne, const RGBColour& cTwo) noexcept
-{
-	cOne.r *= cTwo.r;
-	cOne.g *= cTwo.g;
-	cOne.b *= cTwo.b;
-	cOne.a *= cTwo.a;
+	inline RGBColour& operator*(RGBColour& cOne, const RGBColour& cTwo) noexcept
+	{
+		cOne.r *= cTwo.r;
+		cOne.g *= cTwo.g;
+		cOne.b *= cTwo.b;
+		cOne.a *= cTwo.a;
 
-	return cOne;
-}
+		return cOne;
+	}
 
-inline RGBColour& operator*=(RGBColour& cOne, const uint8_t inc) noexcept
-{
-	cOne.r *= inc;
-	cOne.g *= inc;
-	cOne.b *= inc;
-	cOne.a *= inc;
+	inline RGBColour& operator*=(RGBColour& cOne, const uint8_t inc) noexcept
+	{
+		cOne.r *= inc;
+		cOne.g *= inc;
+		cOne.b *= inc;
+		cOne.a *= inc;
 
-	return cOne;
-}
+		return cOne;
+	}
 
-inline RGBColour& operator/=(RGBColour& cOne, const RGBColour& cTwo) noexcept
-{
-	cOne.r /= cTwo.r;
-	cOne.g /= cTwo.g;
-	cOne.b /= cTwo.b;
-	cOne.a /= cTwo.a;
+	inline RGBColour& operator/=(RGBColour& cOne, const RGBColour& cTwo) noexcept
+	{
+		cOne.r /= cTwo.r;
+		cOne.g /= cTwo.g;
+		cOne.b /= cTwo.b;
+		cOne.a /= cTwo.a;
 
-	return cOne;
-}
+		return cOne;
+	}
 
-inline RGBColour& operator/=(RGBColour& cOne, const uint8_t inc) noexcept
-{
-	cOne.r /= inc;
-	cOne.g /= inc;
-	cOne.b /= inc;
-	cOne.a /= inc;
+	inline RGBColour& operator/=(RGBColour& cOne, const uint8_t inc) noexcept
+	{
+		cOne.r /= inc;
+		cOne.g /= inc;
+		cOne.b /= inc;
+		cOne.a /= inc;
 
-	return cOne;
-}
+		return cOne;
+	}
 #pragma endregion
 
 #pragma region RGBColour Functions
-inline void MaxToOne(RGBColour& colour) noexcept
-{
-	uint8_t max{ std::max(colour.r.v, std::max(colour.g.v, colour.b.v)) };
+	inline void MaxToOne(RGBColour& colour) noexcept
+	{
+		const uint8_t max{ std::max(colour.r.v, std::max(colour.g.v, colour.b.v)) };
 
-	colour /= max;
-}
+		colour /= max;
+	}
 #pragma endregion
 }
