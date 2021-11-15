0119f949bda104415a876eda3dfc950d6e05ae74
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-11-16 00:55:47 +0100

    added more documentation

--- Math/Point.h
+++ Math/Point.h
@@ -7,6 +7,14 @@ namespace Integrian2D
 	/* This is the base class representing a point
 	   I suggest using the provided Point2f type, but others can be created using this class */
 
+	/* Available functions:
+		Type DistanceSquared(const Point<P, Type>& p1, const Point<P, Type>& p2) 
+		=>		Returns the squared distance between the two provided points
+		
+		Type Distance(const Point<P, Type>& p1, const Point<P, Type>& p2)
+		=>		Returns the distance between the two provided points
+		*/
+
 	template<int P, typename Type>
 	struct Point
 	{
