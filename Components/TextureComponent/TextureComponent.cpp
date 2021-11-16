0d1b76636e4c1e71fb6ebcdd59e32aee1e51b1ad
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-11-16 01:34:52 +0100

    fixed TextureComponent::Clone

--- Components/TextureComponent/TextureComponent.cpp
+++ Components/TextureComponent/TextureComponent.cpp
@@ -52,7 +52,13 @@ namespace Integrian2D
 
 	Component* TextureComponent::Clone(GameObject* pOwner) noexcept
 	{
-		return new TextureComponent{ pOwner, m_pTexture };
+		TextureComponent* pTextureComponent{ new TextureComponent{pOwner} };
+
+		pTextureComponent->m_DestRect = m_DestRect;
+		pTextureComponent->m_SourceRect = m_SourceRect;
+		pTextureComponent->m_pTexture = m_pTexture;
+
+		return pTextureComponent;
 	}
 
 	void TextureComponent::SetTexture(Texture* const pTexture) noexcept
