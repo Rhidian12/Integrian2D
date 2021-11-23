d447ba9bdf8fc4d1f0fd82edf8d8b2f4bc60e244
Rhidian De Wit <rhidiandewit@gmail.com>
2021-11-24 00:56:19 +0100

    edge constructor added

--- Components/RigidbodyComponent/RigidbodyComponent.h
+++ Components/RigidbodyComponent/RigidbodyComponent.h
@@ -35,17 +35,24 @@ namespace Integrian2D
 #pragma region Templated Constructors
 		template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Circle, bool> = true>
 		RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const float circleRadius, const float density = 1.f, const float friction = 0.1f);
+		template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Edge, bool> = true>
+		RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const PLinef& edge, const float density = 1.f, const float friction = 0.1f);
 #pragma endregion
 
 		Component* Clone(GameObject* const pOwner) noexcept override;
 
 	private:
 		virtual void RootStart() noexcept override;
+
+		RigidbodyType m_RigidbodyType;
+		RigidbodyShape m_RigidbodyShape;
 	};
 
 	template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Circle, bool>>
 	RigidbodyComponent::RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const float circleRadius, const float density, const float friction)
 		: Component{ pOwner }
+		, m_RigidbodyType{ rigidBodyType }
+		, m_RigidbodyShape{ _RigidBodyShape }
 	{
 		ASSERT(!Utils::AreEqual(density, 0.f), "RigidbodyComponent(RigidbodyShape::Circle) > Density may not be 0!");
 
@@ -56,6 +63,31 @@ namespace Integrian2D
 
 		b2CircleShape shape{};
 		shape.m_radius = circleRadius;
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &shape;
+		fixtureDef.density = density;
+		fixtureDef.friction = friction;
+
+		Locator::GetInstance()->GetPhysicsEngine()->AddPhysicsComponent(this, bodyDef, fixtureDef);
+	}
+
+	template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Edge, bool>>
+	RigidbodyComponent::RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const PLinef& edge, const float density, const float friction)
+		: Component{ pOwner }
+		, m_RigidbodyType{ rigidBodyType }
+		, m_RigidbodyShape{ _RigidBodyShape }
+	{
+		ASSERT(!Utils::AreEqual(density, 0.f), "RigidbodyComponent(RigidbodyShape::Edge) > Density may not be 0!");
+
+		b2BodyDef bodyDef{};
+
+		bodyDef.type = static_cast<b2BodyType>(rigidBodyType);
+		bodyDef.position.Set(pOwner->pTransform->GetWorldPosition().x, pOwner->pTransform->GetWorldPosition().y);
+
+		b2EdgeShape shape{};
+		shape.m_vertex1 = edge.begin;
+		shape.m_vertex2 = edge.end;
 
 		b2FixtureDef fixtureDef;
 		fixtureDef.shape = &shape;
