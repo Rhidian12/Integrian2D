fd6af31f877d6136cfe829bac6b617cd177c7d45
Rhidian De Wit <rhidiandewit@gmail.com>
2021-11-24 01:16:43 +0100

    backup

--- Components/RigidbodyComponent/RigidbodyComponent.h
+++ Components/RigidbodyComponent/RigidbodyComponent.h
@@ -7,6 +7,7 @@
 #include "../TransformComponent/TransformComponent.h"
 
 #include <type_traits>
+#include <vector>
 #include <box2d.h>
 
 namespace Integrian2D
@@ -37,6 +38,10 @@ namespace Integrian2D
 		RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const float circleRadius, const float density = 1.f, const float friction = 0.1f);
 		template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Edge, bool> = true>
 		RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const PLinef& edge, const float density = 1.f, const float friction = 0.1f);
+		template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Chain, bool> = true>
+		RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const std::vector<Point2f>& points, const float density = 1.f, const float friction = 0.1f);
+		template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Polygon, bool> = true>
+		RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const std::vector<Point2f>& points, const float density = 1.f, const float friction = 0.1f);
 #pragma endregion
 
 		Component* Clone(GameObject* const pOwner) noexcept override;
@@ -88,6 +93,66 @@ namespace Integrian2D
 		b2EdgeShape shape{};
 		shape.m_vertex1 = edge.begin;
 		shape.m_vertex2 = edge.end;
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &shape;
+		fixtureDef.density = density;
+		fixtureDef.friction = friction;
+
+		Locator::GetInstance()->GetPhysicsEngine()->AddPhysicsComponent(this, bodyDef, fixtureDef);
+	}
+
+	template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Polygon, bool>>
+	RigidbodyComponent::RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const std::vector<Point2f>& points, const float density, const float friction)
+		: Component{ pOwner }
+		, m_RigidbodyType{ rigidBodyType }
+		, m_RigidbodyShape{ _RigidBodyShape }
+	{
+		ASSERT(!Utils::AreEqual(density, 0.f), "RigidbodyComponent(RigidbodyShape::Polygon) > Density may not be 0!");
+		ASSERT(points.size() <= b2_maxPolygonVertices, std::string{ "RigidbodyComponent(RigidbodyShape::Polygon) > There may only be " } + std::to_string(b2_maxPolygonVertices) + " vertices in a polygon");
+
+		b2BodyDef bodyDef{};
+
+		bodyDef.type = static_cast<b2BodyType>(rigidBodyType);
+		bodyDef.position.Set(pOwner->pTransform->GetWorldPosition().x, pOwner->pTransform->GetWorldPosition().y);
+
+		b2PolygonShape shape{};
+		b2Vec2* pPoints[b2_maxPolygonVertices]{};
+
+		for (size_t i{}; i < points.size(); ++i)
+			pPoints[i] = b2Vec2{ points[i].x, points[i].y };
+
+		shape.Set(pPoints, points.size());
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &shape;
+		fixtureDef.density = density;
+		fixtureDef.friction = friction;
+
+		Locator::GetInstance()->GetPhysicsEngine()->AddPhysicsComponent(this, bodyDef, fixtureDef);
+	}
+
+	template<RigidbodyShape _RigidBodyShape, std::enable_if_t<_RigidBodyShape == RigidbodyShape::Chain, bool>>
+	RigidbodyComponent::RigidbodyComponent(GameObject* const pOwner, const RigidbodyType rigidBodyType, const std::vector<Point2f>& points, const float density, const float friction)
+		: Component{ pOwner }
+		, m_RigidbodyType{ rigidBodyType }
+		, m_RigidbodyShape{ _RigidBodyShape }
+	{
+		ASSERT(!Utils::AreEqual(density, 0.f), "RigidbodyComponent(RigidbodyShape::Polygon) > Density may not be 0!");
+		ASSERT(points.size() <= b2_maxPolygonVertices, std::string{ "RigidbodyComponent(RigidbodyShape::Polygon) > There may only be " } + std::to_string(b2_maxPolygonVertices) + " vertices in a polygon");
+
+		b2BodyDef bodyDef{};
+
+		bodyDef.type = static_cast<b2BodyType>(rigidBodyType);
+		bodyDef.position.Set(pOwner->pTransform->GetWorldPosition().x, pOwner->pTransform->GetWorldPosition().y);
+
+		b2ChainShape shape{};
+		b2Vec2* pPoints[b2_maxPolygonVertices]{};
+
+		for (size_t i{}; i < points.size(); ++i)
+			pPoints[i] = b2Vec2{ points[i].x, points[i].y };
+
+		shape.Set(pPoints, points.size());
 
 		b2FixtureDef fixtureDef;
 		fixtureDef.shape = &shape;
