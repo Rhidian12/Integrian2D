ec051a80e06713565128d48177af50968917285a
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-12-05 16:37:27 +0100

    Blackboard made

--- AI/Blackboard/Blackboard.h
+++ AI/Blackboard/Blackboard.h
@@ -1,10 +1,66 @@
 #pragma once
 
+#include "../../Utils/Utils.h"
+
+#include <unordered_map>
+#include <string>
+#include <memory>
+
 namespace Integrian2D
 {
+	class IBlackboardData
+	{
+	public:
+		virtual ~IBlackboardData() = default;
+	};
+
+	template<typename Type>
+	class BlackboardData final : public IBlackboardData
+	{
+	public:
+		BlackboardData<Type>(const Type& data)
+			: m_Data{ data }
+		{}
+
+		Type GetData() const noexcept
+		{
+			return m_Data;
+		}
+
+	private:
+		Type m_Data;
+	};
+
 	class Blackboard final
 	{
 	public:
+		template<typename Type>
+		void AddData(const std::string& id, const Type& data) noexcept
+		{
+			m_pData.insert(std::make_pair(id, std::make_shared<Type>(data)));
+		}
 
+		template<typename Type>
+		const Type& GetData(const std::string& id) const noexcept
+		{
+			auto it{ m_pData.find(id) };
+
+			ASSERT(it != m_pData.cend(), std::string{ "Blackboard::GetData() > the string with id" } + id + " was not found!");
+			
+			return std::static_pointer_cast<Type>(it->second)->GetData();
+		}
+
+		template<typename Type>
+		void ChangeData(const std::string& id, const Type& data) noexcept
+		{
+			auto it{ m_pData.find(id) };
+			
+			ASSERT(it != m_pData.cend(), std::string{ "Blackboard::ChangeData() > the string with id" } + id + " was not found!");
+
+			m_pData[id] = data;
+		}
+
+	private:
+		std::unordered_map<std::string, std::shared_ptr<IBlackboardData>> m_pData;
 	};
 }
