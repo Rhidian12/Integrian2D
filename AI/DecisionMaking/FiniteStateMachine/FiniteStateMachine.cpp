2046185fc819977e05a6f3c92186d99bb5c5e09e
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-12-05 14:45:45 +0100

    backup

--- AI/DecisionMaking/FiniteStateMachine/FiniteStateMachine.cpp
+++ AI/DecisionMaking/FiniteStateMachine/FiniteStateMachine.cpp
@@ -2,5 +2,65 @@
 
 namespace Integrian2D
 {
+	FiniteStateMachine::FiniteStateMachine(AIComponent* const pAIComponent, State* const pStartState)
+		: BaseDecisionMaking{ pAIComponent }
+		, m_pCurrentState{ pStartState }
+	{
+		m_pStates.push_back(pStartState);
+	}
 
+	void FiniteStateMachine::AddState(State* const pState) noexcept
+	{
+		m_pStates.push_back(pState);
+	}
+
+	void FiniteStateMachine::AddTransition(Transition* const pTransition) noexcept
+	{
+		m_pTransitions.push_back(pTransition);
+	}
+
+	void FiniteStateMachine::Update()
+	{
+	}
+
+	State::State(AIComponent* const pAIComponent, FiniteStateMachine* const pFSM, const Action& action)
+		: BaseDecisionMaking{ pAIComponent }
+		, m_pFSM{ pFSM }
+		, m_Action{ action }
+		, m_CurrentState{ BehaviourState::Failure }
+	{}
+
+	void State::Update()
+	{
+		if (m_Action)	
+			m_CurrentState = m_Action();
+	}
+
+	FiniteStateMachine* const State::GetFiniteStateMachine() const noexcept
+	{
+		return m_pFSM;
+	}
+
+	const State::Action& State::GetAction() const noexcept
+	{
+		return m_Action;
+	}
+
+	const BehaviourState State::GetCurrentState() const noexcept
+	{
+		return m_CurrentState;
+	}
+
+	Transition::Transition(FiniteStateMachine* const pFSM, State* const pFrom, State* const pTo, const Predicate& predicate)
+		: m_pFSM{ pFSM }
+		, m_Predicate{ predicate }
+		, m_pFrom{ pFrom }
+		, m_pTo{ pTo }
+	{
+	}
+
+	bool Transition::Execute()
+	{
+		return m_Predicate();
+	}
 }
--- AI/DecisionMaking/FiniteStateMachine/FiniteStateMachine.h
+++ AI/DecisionMaking/FiniteStateMachine/FiniteStateMachine.h
@@ -2,11 +2,62 @@
 
 #include "../BaseDecisionMaking/BaseDecisionMaking.h"
 
+#include <functional>
+#include <vector>
+
 namespace Integrian2D
 {
+	class FiniteStateMachine;
+
+	class State final : public BaseDecisionMaking
+	{
+	public:
+		using Action = std::function<BehaviourState()>;
+
+		State(AIComponent* const pAIComponent, FiniteStateMachine* const pFSM, const Action& action);
+
+		virtual void Update() override;
+
+		FiniteStateMachine* const GetFiniteStateMachine() const noexcept;
+		const Action& GetAction() const noexcept;
+		const BehaviourState GetCurrentState() const noexcept;
+
+	private:
+		FiniteStateMachine* m_pFSM;
+		Action m_Action;
+		BehaviourState m_CurrentState;
+	};
+
+	class Transition final
+	{
+	public:
+		using Predicate = std::function<bool()>;
+
+		Transition(FiniteStateMachine* const pFSM, State* const pFrom, State* const pTo, const Predicate& predicate);
+
+		bool Execute();
+
+	private:
+		FiniteStateMachine* m_pFSM;
+		Predicate m_Predicate;
+		State* m_pFrom;
+		State* m_pTo;
+	};
+
 	class FiniteStateMachine final : public BaseDecisionMaking
 	{
 	public:
-		FiniteStateMachine(AIComponent* const pAIComponent);
+		FiniteStateMachine(AIComponent* const pAIComponent, State* const pStartState);
+
+		void AddState(State* const pState) noexcept;
+		void AddTransition(Transition* const pTransition) noexcept;
+
+		virtual void Update() override;
+
+	private:
+		std::vector<State*> m_pStates;
+		std::vector<Transition*> m_pTransitions;
+
+		State* m_pCurrentState;
 	};
 }
