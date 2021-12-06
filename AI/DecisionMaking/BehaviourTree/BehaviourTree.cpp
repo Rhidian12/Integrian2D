cf5c8ca8d8811dacdab3827895531500d247af10
Rhidian De Wit <rhidiandewit@gmail.com>
2021-12-06 01:24:56 +0100

    BHT made

--- AI/DecisionMaking/BehaviourTree/BehaviourTree.cpp
+++ AI/DecisionMaking/BehaviourTree/BehaviourTree.cpp
@@ -1,1 +1,99 @@
 #include "BehaviourTree.h"
+
+namespace Integrian2D
+{
+	BehaviourTree::BehaviourTree(AIComponent* const pAIComponent, const std::vector<BehaviourTreeNode*>& nodes)
+		: BaseDecisionMaking{ pAIComponent }
+		, m_Nodes{ nodes }
+		, m_pCurrentNode{}
+	{
+	}
+
+	BehaviourState BehaviourTree::Update(Blackboard* const pBlackboard)
+	{
+		auto it{ m_Nodes.cbegin() };
+		auto end{ m_Nodes.cend() };
+
+		for (; it != end; ++it)
+		{
+			if (m_pCurrentNode)
+				if (m_pCurrentNode != *it)
+					continue;
+
+			switch ((*it)->Execute(pBlackboard))
+			{
+			case BehaviourState::Failure:
+				m_pCurrentNode = nullptr;
+				return BehaviourState::Failure;
+			case BehaviourState::Running:
+				m_pCurrentNode = *it;
+				return BehaviourState::Running;
+			case BehaviourState::Success:
+				if (m_pCurrentNode)
+					m_pCurrentNode = nullptr;
+				break;
+			}
+		}
+
+		return BehaviourState::Success;
+	}
+
+	ActionNode::ActionNode(const Action& action)
+		: m_Action{ action }
+	{
+	}
+
+	BehaviourState ActionNode::Execute(Blackboard* const pBlackboard) const
+	{
+		return m_Action(pBlackboard);
+	}
+
+	ConditionalNode::ConditionalNode(const Predicate& predicate)
+		: m_Predicate{ predicate }
+	{
+	}
+
+	BehaviourState ConditionalNode::Execute(Blackboard* const pBlackboard) const
+	{
+		if (m_Predicate(pBlackboard))
+			return BehaviourState::Success;
+		else
+			return BehaviourState::Failure;
+	}
+
+	InvertedConditionalNode::InvertedConditionalNode(const Predicate& predicate)
+		: m_Predicate{ predicate }
+	{
+	}
+
+	BehaviourState InvertedConditionalNode::Execute(Blackboard* const pBlackboard) const
+	{
+		if (!m_Predicate(pBlackboard))
+			return BehaviourState::Success;
+		else
+			return BehaviourState::Failure;
+	}
+
+	SequenceNode::SequenceNode(const std::vector<BehaviourTreeNode*>& nodes)
+		: m_Nodes{ nodes }
+	{
+	}
+
+	BehaviourState SequenceNode::Execute(Blackboard* const pBlackboard) const
+	{
+		for (const BehaviourTreeNode* const pNode : m_Nodes)
+		{
+			switch (pNode->Execute(pBlackboard))
+			{
+			case BehaviourState::Failure:
+				return BehaviourState::Failure;
+			case BehaviourState::Running:
+				return BehaviourState::Running;
+			case BehaviourState::Success:
+				break;
+			}
+		}
+
+		return BehaviourState::Success;
+	}
+}
--- AI/DecisionMaking/BehaviourTree/BehaviourTree.h
+++ AI/DecisionMaking/BehaviourTree/BehaviourTree.h
@@ -1,5 +1,79 @@
 #pragma once
-class BehaviourTree
-{
-};
 
+#include "../BaseDecisionMaking/BaseDecisionMaking.h"
+
+#include <functional>
+#include <vector>
+
+namespace Integrian2D
+{
+	class BehaviourTreeNode
+	{
+	public:
+		virtual ~BehaviourTreeNode() = default;
+
+		virtual BehaviourState Execute(Blackboard* const) const = 0;
+	};
+
+	class ActionNode final : public BehaviourTreeNode
+	{
+	public:
+		using Action = std::function<BehaviourState(Blackboard* const)>;
+
+		ActionNode(const Action& action);
+
+		virtual BehaviourState Execute(Blackboard* const pBlackboard) const override;
+
+	private:
+		Action m_Action;
+	};
+
+	class ConditionalNode final : public BehaviourTreeNode
+	{
+	public:
+		using Predicate = std::function<bool(Blackboard* const)>;
+
+		ConditionalNode(const Predicate& predicate);
+
+		virtual BehaviourState Execute(Blackboard* const pBlackboard) const override;
+
+	private:
+		Predicate m_Predicate;
+	};
+
+	class InvertedConditionalNode final : public BehaviourTreeNode
+	{
+	public:
+		using Predicate = std::function<bool(Blackboard* const)>;
+
+		InvertedConditionalNode(const Predicate& predicate);
+
+		virtual BehaviourState Execute(Blackboard* const pBlackboard) const override;
+
+	private:
+		Predicate m_Predicate;
+	};
+
+	class SequenceNode final : public BehaviourTreeNode
+	{
+	public:
+		SequenceNode(const std::vector<BehaviourTreeNode*>& nodes);
+
+		virtual BehaviourState Execute(Blackboard* const pBlackboard) const override;
+
+	private:
+		std::vector<BehaviourTreeNode*> m_Nodes;
+	};
+
+	class BehaviourTree final : public BaseDecisionMaking
+	{
+	public:
+		BehaviourTree(AIComponent* const pAIComponent, const std::vector<BehaviourTreeNode*>& nodes);
+
+		virtual BehaviourState Update(Blackboard* const pBlackboard) override;
+
+	private:
+		std::vector<BehaviourTreeNode*> m_Nodes;
+		const BehaviourTreeNode* m_pCurrentNode;
+	};
+}
