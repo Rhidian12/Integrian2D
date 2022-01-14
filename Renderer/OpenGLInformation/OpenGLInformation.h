f87c67d63389a84bef1bea4ab84e1ae851327b8f
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-01-14 15:32:46 +0100

    made OpenGLInformation

--- Renderer/OpenGLInformation/OpenGLInformation.h
+++ Renderer/OpenGLInformation/OpenGLInformation.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "../../Math/TypeDefines.h"
+
+#include <vector>
+
 namespace Integrian2D
 {
 	enum class OpenGLShape
@@ -10,4 +14,63 @@ namespace Integrian2D
 		Circle,
 		Polygon
 	};
+
+	template<OpenGLShape Shape>
+	struct OpenGLInformation final
+	{
+		OpenGLInformation() = default;
+		~OpenGLInformation() = default;
+
+#pragma region AlwaysPresent
+		RGBColour colour{ 255,0,0 };
+		bool isFilled{};
+#pragma endregion
+
+#pragma region SFINAE
+		template<OpenGLShape _Shape = Shape>
+		struct Type
+		{};
+
+		template<>
+		struct Type<OpenGLShape::Rectangle>
+		{
+			PRectf rectangle{};
+		};
+
+		template<>
+		struct Type<OpenGLShape::Triangle>
+		{
+			PTrianglef triangle{};
+		};
+
+		template<>
+		struct Type<OpenGLShape::Line>
+		{
+			PLinef line{};
+			float lineWidth{};
+		};
+
+		template<>
+		struct Type<OpenGLShape::Circle>
+		{
+			Circlef circle{};
+		};
+
+		template<>
+		struct Type<OpenGLShape::Polygon>
+		{
+			std::vector<Point2f> points{};
+			float lineWidth{};
+		};
+#pragma endregion
+
+		OpenGLShape shape{ Shape };
+		Type<Shape> shapeSpecificData{};
+	};
+
+	using OpenGLRectangle	= OpenGLInformation<OpenGLShape::Rectangle>;
+	using OpenGLTriangle	= OpenGLInformation<OpenGLShape::Triangle>;
+	using OpenGLLine		= OpenGLInformation<OpenGLShape::Line>;
+	using OpenGLCircle		= OpenGLInformation<OpenGLShape::Circle>;
+	using OpenGLPolygon		= OpenGLInformation<OpenGLShape::Polygon>;
 }
