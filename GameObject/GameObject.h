952900efa7f50ab5315c071088cb4d1e8d2cfec1
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-02-06 16:10:38 +0100

    added RemoveGameObject

--- GameObject/GameObject.h
+++ GameObject/GameObject.h
@@ -6,13 +6,14 @@
 #include <vector>
 #include <string>
 #include <typeinfo>
+#include <algorithm>
 
 namespace Integrian2D
 {
 	class Component;
 	class TransformComponent;
 
-	/* The GameObject is the class that holds a list of Components, and calls their overridable functions, such as Render() 
+	/* The GameObject is the class that holds a list of Components, and calls their overridable functions, such as Render()
 	   The GameObjects get added to the Scene via Scene::AddGameObject */
 	class GameObject final
 	{
@@ -37,7 +38,7 @@ namespace Integrian2D
 		INTEGRIAN2D_API void Render() const;
 
 		/* This function returns the Component requested by the User. This function will cause a compilation error
-		   if the requested Type is class not derived from Component 
+		   if the requested Type is class not derived from Component
 		   If the requested Type is present, it will return a pointer to the requested component
 		   Otherwise this function returns a nullptr */
 		template<typename Type, typename = std::enable_if_t<std::is_base_of_v<Component, Type>>>
@@ -46,6 +47,12 @@ namespace Integrian2D
 		/* Adds a Component to the GameObject, if it not already present
 		   Duplicate Component Types are allowed, but not the same Component twice */
 		INTEGRIAN2D_API void AddComponent(Component* const pComponent) noexcept;
+
+		/* Removes a component from the GameObject
+			This function will remove ALL Components of the provided type
+			This function does NOT destroy Components! */
+		template<typename Type, typename = std::enable_if_t<std::is_base_of_v<Component, Type>>>
+		void RemoveComponentByType(std::vector<Component*>* pComponents) noexcept;
 
 		/* Sets another GameObject as this GameObject's Child
 		   The GameObject set as Child its parent is set as this GameObject */
@@ -59,11 +66,11 @@ namespace Integrian2D
 		/* Set the GameObject's tag */
 		INTEGRIAN2D_API void SetTag(const std::string& tag) noexcept;
 
-		/* Get this GameObject's parent 
+		/* Get this GameObject's parent
 		   If the GameObject has no parent, this function returns a nullptr */
 		INTEGRIAN2D_API GameObject* const GetParent() const noexcept;
 
-		/* Get the GameObject's Children 
+		/* Get the GameObject's Children
 		   If the GameObject has no Children, this functions returns an empty std::vector */
 		INTEGRIAN2D_API const std::vector<GameObject*>& GetChildren() const noexcept;
 
@@ -99,6 +106,33 @@ namespace Integrian2D
 				return static_cast<Type*>(pC);
 
 		return nullptr;
+	}
+
+	template<typename Type, typename>
+	void GameObject::RemoveComponentByType(std::vector<Component*>* pComponents) noexcept
+	{
+		const std::type_info& typeInfo{ typeid(Type) };
+
+		if (pComponents)
+		{
+			m_pComponents.erase(std::remove_if(m_pComponents.begin(), m_pComponents.end(), [&typeInfo, &pComponents](Component* const pC)->bool
+				{
+					if (typeid(*pC) == typeInfo)
+					{
+						(*pComponents).push_back(pC);
+						return true;
+					}
+					else
+						return false;
+				}), m_pComponents.end());
+		}
+		else
+		{
+			m_pComponents.erase(std::remove_if(m_pComponents.begin(), m_pComponents.end(), [&typeInfo](Component* const pC)->bool
+				{
+					return typeid(*pC) == typeInfo;
+				}), m_pComponents.end());
+		}
 	}
 }
 
